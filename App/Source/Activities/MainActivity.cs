cf09d42024420b0f6784a99144272cf2ec16bfa0
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-05-28 23:50:24 -0400

    Add startup permission begging

--- App/Source/Activities/MainActivity.cs
+++ App/Source/Activities/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using Android;
 using Android.App;
 using Android.Content;
+using Android.Net;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -9,6 +11,7 @@ using AndroidX.AppCompat.App;
 using MelonLoaderInstaller.App.Adapters;
 using MelonLoaderInstaller.App.Models;
 using MelonLoaderInstaller.App.Utilities;
+using AlertDialog = AndroidX.AppCompat.App.AlertDialog;
 
 namespace MelonLoaderInstaller.App.Activities
 {
@@ -35,12 +38,61 @@ namespace MelonLoaderInstaller.App.Activities
             listView.Adapter = adapter;
             listView.OnItemClickListener = this;
 
+            TryRequestPermissions();
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (grantResults.Length > 0 && grantResults[0] == Android.Content.PM.Permission.Granted)
+            {
+                RequestInstallUnknownSources();
+            }
+            else
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder
+                        .SetTitle("Permissions Issue")
+                        .SetMessage("Lemon needs to be granted storage permissions to function!")
+                        .SetPositiveButton("Setup", (o, di) => TryRequestPermissions())
+                        .SetIcon(Android.Resource.Drawable.IcDialogAlert);
+
+                AlertDialog alert = builder.Create();
+                alert.SetCancelable(false);
+                alert.Show();
+            }
+        }
+
+        public void TryRequestPermissions()
+        {
+            bool canRead = CheckSelfPermission(Manifest.Permission.ReadExternalStorage) == Android.Content.PM.Permission.Granted;
+            bool canWrite = CheckSelfPermission(Manifest.Permission.ReadExternalStorage) == Android.Content.PM.Permission.Granted;
+
+            if (!canRead || !canWrite)
+            {
+                RequestPermissions(new string[]
+                {
+                    Manifest.Permission.ReadExternalStorage,
+                    Manifest.Permission.WriteExternalStorage,
+                }, 100);
+            }
+        }
+
+        public void RequestInstallUnknownSources()
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder
+                    .SetTitle("Install Permission")
+                    .SetMessage("Lemon needs permission to install apps from unknown sources to function!")
+                    .SetPositiveButton("Setup", (o, di) => StartActivity(new Intent(Android.Provider.Settings.ActionManageUnknownAppSources, Uri.Parse("package:" + PackageName))))
+                    .SetIcon(Android.Resource.Drawable.IcDialogAlert);
+
+            AlertDialog alert = builder.Create();
+            alert.SetCancelable(false);
+            alert.Show();
         }
 
         public void OnItemClick(AdapterView parent, View view, int position, long id)
