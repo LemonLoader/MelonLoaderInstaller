8103eba86560a0ef5bb73ee70556d06296eafb86
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-06-04 12:04:50 -0400

    Rename Class1; Create PatchArguments

---
+++ Core/PatchArguments.cs
@@ -0,0 +1,27 @@
+﻿namespace MelonLoaderInstaller.Core
+{
+    public class PatchArguments
+    {
+        public string TargetApkPath;
+        public string LibraryApkPath;
+
+        public string OutputApkPath;
+
+        public string TempDirectory;
+
+        public string LemonDataPath;
+        public string Il2CppEtcPath;
+        public string UnityDependenciesPath;
+
+        public PatchArguments(string targetApkPath, string libraryApkPath, string outputApkPath, string tempDirectory, string lemonDataPath, string il2CppEtcPath, string unityDependenciesPath)
+        {
+            TargetApkPath = targetApkPath;
+            LibraryApkPath = libraryApkPath;
+            OutputApkPath = outputApkPath;
+            TempDirectory = tempDirectory;
+            LemonDataPath = lemonDataPath;
+            Il2CppEtcPath = il2CppEtcPath;
+            UnityDependenciesPath = unityDependenciesPath;
+        }
+    }
+}
---
+++ Core/Patcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MelonLoaderInstaller.Core
+{
+    public static class Patcher
+    {
+        public static void Run(PatchArguments arguments)
+        {
+
+        }
+    }
+}
