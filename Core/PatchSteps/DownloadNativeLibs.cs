dd70271e5fab25f902e6d5d1bfef06223eb3ef4a
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-06-04 18:27:47 -0400

    Implement DownloadNativeLibs

--- Core/PatchSteps/DownloadNativeLibs.cs
+++ Core/PatchSteps/DownloadNativeLibs.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
+using System.Net;
 
 namespace MelonLoaderInstaller.Core.PatchSteps
 {
     internal class DownloadNativeLibs : IPatchStep
     {
+        private const string REPO_BASE = "https://github.com/LemonLoader/NativeLibraries/raw/main/";
+
         public bool Run(Patcher patcher)
         {
+            string packageName = Path.GetFileName(patcher._args.TempDirectory);
+            patcher._logger.Log($"Checking for extra native libraries [ {packageName} ]");
+
+            try
+            {
+                using WebClient client = new WebClient();
+                string outPath = Path.Combine(patcher._args.TempDirectory, "extraLibraries.zip");
+                client.DownloadFile(REPO_BASE + packageName + ".zip", outPath);
+                patcher._logger.Log("Downloaded");
+            }
+            catch (WebException)
+            {
+                patcher._logger.Log("No extra libraries found");
+            }
+
             return true;
         }
     }
