d66ace57b22e5aa813de74de156663bd16246f3f
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-06-04 20:06:25 -0400

    Implement ExtractDependencies

--- Core/PatchSteps/ExtractDependencies.cs
+++ Core/PatchSteps/ExtractDependencies.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO.Compression;
+using System.IO;
 
 namespace MelonLoaderInstaller.Core.PatchSteps
 {
@@ -8,6 +7,34 @@ namespace MelonLoaderInstaller.Core.PatchSteps
     {
         public bool Run(Patcher patcher)
         {
+            patcher._logger.Log("Extracting Dependencies");
+
+            {
+                using FileStream zipStream = new FileStream(patcher._args.LemonDataPath, FileMode.Open);
+                using ZipArchive archive = new ZipArchive(zipStream, ZipArchiveMode.Read);
+                archive.ExtractToDirectory(patcher._info.LemonDataDirectory);
+            }
+
+            patcher._logger.Log("Extracting il2cpp/etc");
+
+            {
+                using FileStream zipStream = new FileStream(patcher._args.Il2CppEtcPath, FileMode.Open);
+                using ZipArchive archive = new ZipArchive(zipStream, ZipArchiveMode.Read);
+                archive.ExtractToDirectory(patcher._info.LemonDataDirectory);
+            }
+
+            patcher._logger.Log("Extracting Libraries");
+
+            {
+                string outPath = Path.Combine(patcher._args.TempDirectory, "extraLibraries.zip");
+                if (!File.Exists(outPath))
+                    return true;
+
+                using FileStream zipStream = new FileStream(outPath, FileMode.Open);
+                using ZipArchive archive = new ZipArchive(zipStream, ZipArchiveMode.Read);
+                archive.ExtractToDirectory(patcher._info.LemonDataDirectory);
+            }
+
             return true;
         }
     }
