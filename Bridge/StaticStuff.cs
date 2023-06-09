0fcc1f5aecdecda02b2db1b47e3b850ff8ea804f
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-06-09 14:11:01 -0400

    Copy over the ADBBridge project

---
+++ Bridge/StaticStuff.cs
@@ -0,0 +1,7 @@
+﻿namespace LemonADBBridge
+{
+    internal static class StaticStuff
+    {
+        public static string ADBPath;
+    }
+}
---
+++ Bridge/StringReceiver.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
+using SharpAdbClient;
+using SharpAdbClient.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LemonADBBridge
+{
+    internal class StringReceiver : MultiLineReceiver
+    {
+        /// <summary>
+        /// A <see cref="StringBuilder"/> which receives all output from the device.
+        /// </summary>
+        private readonly StringBuilder output = new StringBuilder();
+
+        /// <summary>
+        /// Gets a <see cref="string"/> that represents the current <see cref="StringReceiver"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string"/> that represents the current <see cref="StringReceiver"/>.
+        /// </returns>
+        public override string ToString()
+        {
+            return this.output.ToString();
+        }
+
+        /// <summary>
+        /// Processes the new lines.
+        /// </summary>
+        /// <param name="lines">The lines.</param>
+        protected override void ProcessNewLines(IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line) || line.StartsWith("#") || line.StartsWith("$"))
+                {
+                    continue;
+                }
+
+                output.AppendLine(line);
+            }
+        }
+    }
+}
