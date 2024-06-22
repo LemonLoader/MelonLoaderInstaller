42533b02e66c85b37048d6ebcb53f81ef352e645
Trevor <me@trev.app>
2024-06-22 19:17:52 -0400

    Add Unity apps to grid on Android

--- App/MainPageViewModel.cs
+++ App/MainPageViewModel.cs
@@ -1,65 +1,18 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows.Input;
-using UraniumUI;
+﻿using MelonLoader.Installer.App.Utils;
+using System.Collections.ObjectModel;
 
 namespace MelonLoader.Installer.App;
 public class MainPageViewModel : BindableObject
 {
-    public ObservableCollection<TodoItem> Items { get; protected set; } = new ObservableCollection<TodoItem>();
-
-    public ObservableCollection<TodoItem> SelectedItems { get; set; } = new ObservableCollection<TodoItem>();
-
-    private TodoItem newItem = new();
-    public TodoItem NewItem { get => newItem; set { newItem = value; OnPropertyChanged(); } }
-
-    public ICommand AddNewItemCommand { get; protected set; }
-
-    public ICommand RemoveSelectedItemsCommand { get; protected set; }
+    public ObservableCollection<UnityApplicationFinder.Data> Items { get; protected set; } = [];
 
     public MainPageViewModel()
     {
-        if (Items.Count == 0)
+        // todo: put this on a separate thread to load the ui asap
+        var apps = UnityApplicationFinder.Find();
+        foreach (var app in apps)
         {
-            Items.Add(new TodoItem { Content = "Throw away the rubbish", Type = TodoItem.TodoItemType.Personal });
-            Items.Add(new TodoItem { Content = "Attend the meeting today\n11:00AM", Type = TodoItem.TodoItemType.Work });
-            Items.Add(new TodoItem { Content = "Prepare presentation for new project", Type = TodoItem.TodoItemType.Work });
-            Items.Add(new TodoItem { Content = "Spend time with family", Type = TodoItem.TodoItemType.Family });
-            Items.Add(new TodoItem { Content = "Complete the puzzle", Type = TodoItem.TodoItemType.Hobby });
-            Items.Add(new TodoItem { Content = "Don't forget to call dad", Type = TodoItem.TodoItemType.Family });
+            Items.Add(app);
         }
-
-        AddNewItemCommand = new Command(() =>
-        {
-            Items.Insert(0, NewItem);
-            NewItem = new();
-        });
-
-        RemoveSelectedItemsCommand = new Command(() =>
-        {
-            foreach (var item in SelectedItems)
-            {
-                Items.Remove(item);
-            }
-        });
-    }
-}
-
-public class TodoItem : UraniumBindableObject
-{
-    public string Content { get; set; }
-
-    private bool isDone;
-    public bool IsDone { get => isDone; set => SetProperty(ref isDone,value); }
-
-    public TodoItemType Type { get; set; }
-
-    public static TodoItemType[] AvailableTypes => Enum.GetValues(typeof(TodoItemType)) as TodoItemType[];
-
-    public enum TodoItemType
-    {
-        Personal,
-        Work,
-        Hobby,
-        Family
     }
 }
---
+++ App/Utils/UnityApplicationFinder.cs
@@ -0,0 +1,74 @@
+﻿#if ANDROID
+using Android.Content.PM;
+#endif
+using UraniumUI;
+
+namespace MelonLoader.Installer.App.Utils
+{
+    public static class UnityApplicationFinder
+    {
+#if ANDROID
+        public static List<Data> Find()
+        {
+            PackageManager pm = Platform.CurrentActivity!.PackageManager ?? throw new Exception("PackageManager is null, how does this happen?");
+            IList<ApplicationInfo> allPackages = pm.GetInstalledApplications(PackageInfoFlags.MetaData);
+
+            List<Data> unityApps = [];
+
+            foreach (ApplicationInfo package in allPackages)
+            {
+                if (package.NativeLibraryDir == null)
+                    continue;
+
+                try
+                {
+                    bool isUnity = Directory.GetFiles(package.NativeLibraryDir).Any(f => f.Contains("libunity.so") && f.Contains("libil2cpp.so"));
+                    bool hasPlayer = pm.GetLaunchIntentForPackage(package.PackageName!)?.Component?.ClassName?.Contains("UnityPlayer") ?? false;
+                    if (!isUnity && !hasPlayer)
+                        continue;
+                }
+                catch (IOException)
+                {
+                    // This probably means that it's a special app that we can't access
+                    continue;
+                }
+
+                unityApps.Add(new Data(package.PackageName!));
+            }
+
+            return unityApps;
+        }
+
+        public static Data FromPackageName(string packageName)
+        {
+            PackageManager pm = Platform.CurrentActivity!.PackageManager ?? throw new Exception("PackageManager is null, how does this happen?");
+            ApplicationInfo packageInfo = pm.GetApplicationInfo(packageName, PackageInfoFlags.MetaData);
+            return new Data(packageInfo.Name!);
+        }
+#else
+        // TODO: windows support
+
+        public static List<Data> Find()
+        {
+            List<Data> unityApps = [new("a"), new("b"), new("c"), new("d")];
+
+            return unityApps;
+        }
+
+        public static Data FromPackageName(string packageName)
+        {
+            return new Data("");
+        }
+#endif
+
+        public class Data : UraniumBindableObject
+        {
+            public string AppName { get; private set; }
+
+            public Data(string appName)
+            {
+                AppName = appName;
+            }
+        }
+    }
+}
