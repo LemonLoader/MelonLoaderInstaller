866a708cd7c5ce231a45e8989f495a4be5f6a753
Trevor <me@trev.app>
2024-07-21 19:42:17 -0400

    Add status to app data

--- App/Utils/UnityApplicationFinder.cs
+++ App/Utils/UnityApplicationFinder.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
+
 #if ANDROID
 using Android.Content.PM;
-using Android.Graphics;
 using Android.Graphics.Drawables;
 #endif
 
@@ -20,9 +20,12 @@ namespace MelonLoader.Installer.App.Utils
                 if (package.NativeLibraryDir == null)
                     continue;
 
+                string[]? libs = null;
+
                 try
                 {
-                    bool isUnity = Directory.GetFiles(package.NativeLibraryDir).Any(f => f.Contains("libunity.so") && f.Contains("libil2cpp.so"));
+                    libs = Directory.GetFiles(package.NativeLibraryDir);
+                    bool isUnity = libs.Any(f => f.Contains("libunity.so")) && libs.Any(f => f.Contains("libil2cpp.so"));
                     bool hasPlayer = pm.GetLaunchIntentForPackage(package.PackageName!)?.Component?.ClassName?.Contains("UnityPlayer") ?? false;
                     if (!isUnity && !hasPlayer)
                         continue;
@@ -35,6 +38,8 @@ namespace MelonLoader.Installer.App.Utils
 
                 string name = pm.GetApplicationLabel(package);
 
+                Status status = GetStatusFromInfo(package, libs);
+
                 byte[]? iconData = null;
                 Drawable? iconDrawable = package.LoadIcon(pm);
                 if (iconDrawable != null)
@@ -42,7 +47,7 @@ namespace MelonLoader.Installer.App.Utils
                     int width = iconDrawable.IntrinsicWidth > 0 ? iconDrawable.IntrinsicWidth : 128;
                     int height = iconDrawable.IntrinsicHeight > 0 ? iconDrawable.IntrinsicHeight : 128;
 
-                    Bitmap bitmap = Bitmap.CreateBitmap(width, height, Bitmap.Config.Argb8888!);
+                    Android.Graphics.Bitmap bitmap = Android.Graphics.Bitmap.CreateBitmap(width, height, Android.Graphics.Bitmap.Config.Argb8888!);
 
                     if (iconDrawable is BitmapDrawable bitmapDrawable)
                     {
@@ -51,44 +56,44 @@ namespace MelonLoader.Installer.App.Utils
 
                     iconDrawable.SetBounds(0, 0, width, height);
 
-                    Canvas canvas = new(bitmap);
+                    Android.Graphics.Canvas canvas = new(bitmap);
                     iconDrawable.Draw(canvas);
 
                     using MemoryStream ms = new();
-                    bitmap.Compress(Bitmap.CompressFormat.Png!, 100, ms);
+                    bitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Png!, 100, ms);
                     iconData = ms.ToArray();
 
                     bitmap.Recycle();
                 }
 
-                yield return new Data(name, package.PackageName!, iconData);
+                yield return new Data(name, package.PackageName!, status, iconData);
             }
 #else
                 List<Data> unityApps = [
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app"),
-                    new("Test App", "com.veryreal.app")];
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unsupported),
+                    new("Test App", "com.veryreal.app", Status.Patched),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unsupported),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Patched),
+                    new("Test App", "com.veryreal.app", Status.Patched),
+                    new("Test App", "com.veryreal.app", Status.Unsupported),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unsupported),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Patched),
+                    new("Test App", "com.veryreal.app", Status.Patched),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unpatched),
+                    new("Test App", "com.veryreal.app", Status.Unsupported),
+                    new("Test App", "com.veryreal.app", Status.Unpatched)];
 
                 foreach (var app in unityApps)
                     yield return app;
@@ -101,17 +106,49 @@ namespace MelonLoader.Installer.App.Utils
             PackageManager pm = Platform.CurrentActivity!.PackageManager ?? throw new Exception("PackageManager is null, how does this happen?");
             ApplicationInfo packageInfo = pm.GetApplicationInfo(packageName, PackageInfoFlags.MetaData);
             string name = pm.GetApplicationLabel(packageInfo);
-            return new Data(name, packageInfo.PackageName!);
+            Status status = GetStatusFromInfo(packageInfo);
+            return new Data(name, packageInfo.PackageName!, status);
 #else
-            return new Data("", "");
+            return new Data("", "", Status.Unpatched);
 #endif
         }
+
+#if ANDROID
+        private static Status GetStatusFromInfo(ApplicationInfo info, string[]? libs = null)
+        {
+            libs ??= Directory.GetFiles(info.NativeLibraryDir!);
+
+            Status status = Status.Unpatched;
+            if (!info.NativeLibraryDir?.Contains("arm64") ?? true)
+                status = Status.Unsupported;
+            if (libs.Any(f => f.Contains("libBootstrap.so")) && libs.Any(f => f.Contains("libdobby.so")))
+                status = Status.Patched;
+
+            return status;
+        }
+#endif
 
         public class Data : BindableObject
         {
             public byte[] RawIconData { get; private set; }
             public string AppName { get; private set; }
             public string PackageName { get; private set; }
+            public Status Status { get; private set; }
+
+            public string StatusString => Status == Status.Unpatched ? "" : Status.ToString().ToUpper();
+
+            public Color StatusColor
+            {
+                get
+                {
+                    return Status switch
+                    {
+                        Status.Patched => new(0, 255, 0, 255),
+                        Status.Unsupported => new(255, 0, 0, 255),
+                        _ => new(255, 255, 255, 255),
+                    };
+                }
+            }
 
             private static byte[] PlaceholderIcon
             {
@@ -132,12 +169,20 @@ namespace MelonLoader.Installer.App.Utils
             }
             private static byte[]? _placeholderIcon;
 
-            public Data(string appName, string packageName, byte[]? icon = null)
+            public Data(string appName, string packageName, Status status, byte[]? icon = null)
             {
                 AppName = appName;
                 PackageName = packageName;
+                Status = status;
                 RawIconData = icon ?? PlaceholderIcon;
             }
+        }
+
+        public enum Status
+        {
+            Unpatched,
+            Patched,
+            Unsupported,
         }
     }
 }
