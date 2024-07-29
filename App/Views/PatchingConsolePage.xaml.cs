5588b6ce40b24c3f6990288c67f21245697706d4
Trevor <me@trev.app>
2024-07-29 15:16:09 -0400

    Add log scrollview and auto-scroller

--- App/Views/PatchingConsolePage.xaml.cs
+++ App/Views/PatchingConsolePage.xaml.cs
@@ -4,31 +4,39 @@ namespace MelonLoader.Installer.App.Views;
 
 public partial class PatchingConsolePage : ContentPage
 {
-	public string Log { get => LogLabel.Text; set => LogLabel.Text = value; }
-	public bool BackButtonVisible { get => GoBackButton.IsVisible; set => GoBackButton.IsVisible = value; }
+    public string Log
+    {
+        get => LogLabel.Text;
+        set
+        {
+            LogLabel.Text = value;
+            LogScrollView.ScrollToAsync(LogLabel, ScrollToPosition.End, false);
+        }
+    }
+    public bool BackButtonVisible { get => GoBackButton.IsVisible; set => GoBackButton.IsVisible = value; }
 
-	private Command _goBackCommand;
+    private Command _goBackCommand;
 
-	public PatchingConsolePage()
-	{
-		InitializeComponent();
-		_goBackCommand = new(GoBack);
+    public PatchingConsolePage()
+    {
+        InitializeComponent();
+        _goBackCommand = new(GoBack);
 
-		GoBackButton.IsVisible = false;
+        GoBackButton.IsVisible = false;
         GoBackButton.GestureRecognizers.Add(new TapGestureRecognizer()
-		{
-			Command = _goBackCommand,
+        {
+            Command = _goBackCommand,
         });
-	}
+    }
 
-	private async void GoBack()
-	{
-		Shell.Current.GoToTabOnFirst(nameof(PatchAppPage));
-		await Navigation.PopAsync();
-	}
+    private async void GoBack()
+    {
+        Shell.Current.GoToTabOnFirst(nameof(PatchAppPage));
+        await Navigation.PopAsync();
+    }
 
     protected override bool OnBackButtonPressed()
     {
-		return false;
+        return false;
     }
 }
