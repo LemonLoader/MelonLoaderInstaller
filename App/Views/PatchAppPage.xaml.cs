e42d9f70bbc043fa3939e4bebf4b0cc6fae5a7a0
Trevor <me@trev.app>
2024-08-07 12:45:00 -0400

    Remove obb/data check
    Threw an error on Quest, leading to a freeze then crash

--- App/Views/PatchAppPage.xaml.cs
+++ App/Views/PatchAppPage.xaml.cs
@@ -23,23 +23,7 @@ public partial class PatchAppPage : ContentPage
 
 #if ANDROID
         if (PatchAppPageViewModel.CurrentAppData.Source == UnityApplicationFinder.Source.PackageManager)
-        {
-            string packageName = PatchAppPageViewModel.CurrentAppData.PackageName;
-
-            string basePath = Android.OS.Environment.ExternalStorageDirectory!.AbsolutePath;
-            string dataPath = Path.Combine(basePath, "Android", "data", packageName);
-            string obbPath = Path.Combine(basePath, "Android", "obb", packageName);
-
-            bool hasObbPath = Directory.Exists(obbPath);
-            bool hasObbFiles = hasObbPath && Directory.GetFiles(obbPath, "*.obb").Length != 0;
-
-            bool hasObb = hasObbPath && hasObbFiles;
-
-            if (hasObb)
-            {
-                await PopupHelper.Alert("This app has OBB data which will get deleted during the patching process. This installer does not back up these files, which could result in a non-functional application. Please back up these files before continuing.", "Warning");
-            }
-        }
+            await PopupHelper.Alert("This app may have data which will get deleted during the patching process. This installer does not back up these files, which could result in a non-functional application. Please back up these files before continuing.", "Warning");
 #endif
 
         if (PackageWarningManager.AvailableWarnings.TryGetValue(PatchAppPageViewModel.CurrentAppData.PackageName, out string? warning) && warning != null)
